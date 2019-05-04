1d324ce11f58afd91787afbe3d15a7d06c6a649f
Facchetti <facchetti.MATOS012@gmail.com>
2019-05-04 13:44:44 -0300

    Segundo Teste com o Visual Studio, porém sem funcionalidade por parte do WAMP Server

--- visao/paginas/mapa.visao.php
+++ visao/paginas/mapa.visao.php
@@ -1,1 +1,3 @@
-<h1>Página Mapa</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.3010408302716!2d-48.02337898520609!3d-23.593534168665908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5cc0aa577fd79%3A0x94275cc265f55968!2sAv.+Jo%C3%A3o+Ol%C3%ADmpio+de+Oliveira+-+Vila+Asem%2C+Itapetininga+-+SP!5e0!3m2!1spt-BR!2sbr!4v1556987759466!5m2!1spt-BR!2sbr" 
+width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+</iframe>
